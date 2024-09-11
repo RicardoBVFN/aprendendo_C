1e0f49f06bfb8422b8970f0625750cf4ee3b3dd6
rbvfn profissional <rbvfn.profissional@gmail.com>
2024-09-11 19:37:26 -0300

    Exercício do Beecrowd de matriz do cálculo da distância entre o centro e as bordas

--- exercicios_beecrowd/matriz_padronizada.C
+++ exercicios_beecrowd/matriz_padronizada.C
@@ -1,37 +1,115 @@
-#include <stdio.h> 
+#include <stdio.h>
 
-int numeroMagico; int main(){
+void geradorDeMatriz(int **matrizVazia, int ordemMatriz){
 
-    while(1){
-    
-        scanf("%d", &numeroMagico);
+    int comparador;
 
-        int matriz[numeroMagico][numeroMagico];
-
-        for (int i = 0; i < numeroMagico; i++){
+    for (int i = 0; i < ordemMatriz; i++){
+        
+        for (int j = 0; j < ordemMatriz; j++){
             
-            matriz[i][i] = 1 + 1;
+            if (i > j){
 
-        }
+                comparador = ordemMatriz - i;
 
-        for (int i = 0; i<numeroMagico; i++){
+                if (j < comparador){
+                    
+                    matrizVazia[i][j] = j + 1;
 
-            for (int j = 0; j < numeroMagico; j++){
-                
-                matriz[i][j] = 1;
+                } else{
 
-                if(i == j){
-
-                    matriz[]
+                    matrizVazia[i][j] = comparador;
 
                 }
+                
+
+            } else{
+
+                if (j > i){
+
+                    comparador = ordemMatriz - j;
+
+                    if (i < comparador){
+                        
+                        matrizVazia[i][j] = i + 1;
+
+                    } else{
+
+                        matrizVazia[i][j] = comparador;
+
+                    }
+
+                } else{
+
+                    if (j >= (ordemMatriz / 2)){
+
+                        matrizVazia[i][j] = ordemMatriz - j;
+
+                    } else{
+
+                        matrizVazia[i][j] = j + 1;
+
+                    }
+
+                }
+                
 
             }
-            
 
         }
         
 
     }
+    
+}
+
+void printMatriz(int **matrizPrint, int ordemMatriz){
+
+   for (int i = 0; i < ordemMatriz; i++) {
+        for (int j = 0; j < ordemMatriz; j++) {
+            if (j == 0) {
+                printf("%3d", matrizPrint[i][j]);
+            } else {
+                printf(" %3d", matrizPrint[i][j]);
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+    
+}
+
+int main(){
+
+    int input;
+
+    while (1){
+
+        scanf("%d", &input);
+
+        if (input != 0){
+
+            int matriz[input][input], *matrizPtr[input];
+
+            for (int i = 0; i < input; i++) {
+
+                matrizPtr[i] = matriz[i];
+
+            }
+
+            geradorDeMatriz(matrizPtr, input);
+
+            printMatriz(matrizPtr, input);
+
+
+        } else{
+
+            break;
+
+        }
+
+    }
+
+    return 0;
 
 }
